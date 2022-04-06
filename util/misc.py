23c5ee9b367a38d56a06cb1fbe1ebf11c631aebc
Samar Khanna <samar.khanna@gmail.com>
2022-04-05 17:32:16 -0700

    print world size

--- util/misc.py
+++ util/misc.py
@@ -240,8 +240,8 @@ def init_distributed_mode(args):
 
     torch.cuda.set_device(args.gpu)
     args.dist_backend = 'nccl'
-    print('| distributed init (rank {}): {}, gpu {}'.format(
-        args.rank, args.dist_url, args.gpu), flush=True)
+    print('| distributed init (rank {}): {}, gpu {}, world size {}'.format(
+        args.rank, args.dist_url, args.gpu, args.world_size), flush=True)
     torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
                                          world_size=args.world_size, rank=args.rank)
     torch.distributed.barrier()
